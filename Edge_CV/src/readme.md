48c2f3c697ce493d635c4b8e6cbad658116532bd
Ian Frommer <ian.frommer@gmail.com>
2025-02-02 12:11:57 -0500

    some attempts at updating the ring code

--- Edge_CV/src/readme.md
+++ Edge_CV/src/readme.md
@@ -1,7 +1,24 @@
 ### Info
 **Login credentials**
+Run ring_api.py first.  See below - may need to do this from Anaconda prompt.
+
+
 When you run ring_api.py it checks if there is a test_token.cache file already in the local folder.  If so, it tries to use this.  If it has expired, delete it.  Then running ring_api.py will cause a new login prompt to occur with Python, complete with 2FA, so have your cell phone ready.
 
 After running ring_api.py, you should be able to run ring_my_work_1.py.  
 
 Was getting errors in downloading videos from Son. on 6/7/24. First with 2FA through the code, 2nd just simply logging in on the browser at Son.
+
+Tried updating the code on 10/26/24.  The test.py on the repo now uses asyncio.  This is what is in my ring_api.py now.  1st I got an async error.  Then I ran
+it from an Anaconda prompt and got:
+AttributeError: 'Auth' object has no attribute 'async_fetch_token'
+No idea what this is.
+
+Got it to work by upgrading the ring_doorbell package and then running from Anaconda prompt again:  python ring_api.py
+
+You can also do things from their CLI - see https://github.com/python-ring-doorbell/python-ring-doorbell
+I'm having trouble connecting my old code ring_my_work_1.py to this new way of logging in.
+I can get ring_api.py to run from Spyder by having it launch in its own terminal window with interactivity.
+But I can't get any of the follow-on commands to work then.  
+May be stuck with CLI.
+
--- Edge_CV/src/ring_api.py
+++ Edge_CV/src/ring_api.py
@@ -1,22 +1,29 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue May 16 17:50:19 2023
-
 @author: ifrommer
+RUN THIS 1st TO LOG IN AND SET UP TOKEN FOR FUTURE LOG-INS.
+This is mostly from the library's test.py
+Updating with changes I see there on 10/26/24
 """
+import asyncio # new
 import json
 import getpass
 from pathlib import Path
 from pprint import pprint
 
-from ring_doorbell import Ring, Auth
-from oauthlib.oauth2 import MissingTokenError
+# from ring_doorbell import Ring, Auth # old
+from ring_doorbell import Auth, AuthenticationError, Requires2FAError, Ring #new
+
+# old
+# from oauthlib.oauth2 import MissingTokenError
 
 
-cache_file = Path("test_token.cache")
+#cache_file = Path("test_token.cache")
+user_agent = "IanRing-1.0"  
+cache_file = Path(user_agent + ".token.cache")
 
-
-def token_updated(token):
+def token_updated(token) -> None:
     cache_file.write_text(json.dumps(token))
 
 
@@ -24,7 +31,39 @@ def otp_callback():
     auth_code = input("2FA code: ")
     return auth_code
 
+async def do_auth():
+    username = input("Username: ")
+    password = getpass.getpass("Password: ")
+    auth = Auth(user_agent, None, token_updated)
+    try:
+        await auth.async_fetch_token(username, password)
+    except Requires2FAError:
+        await auth.async_fetch_token(username, password, otp_callback())
+    return auth
 
+
+async def main() -> None:
+    if cache_file.is_file():  # auth token is cached
+        auth = Auth(user_agent, json.loads(cache_file.read_text()), token_updated)
+        ring = Ring(auth)
+        try:
+            await ring.async_create_session()  # auth token still valid
+        except AuthenticationError:  # auth token has expired
+            auth = await do_auth()
+    else:
+        auth = await do_auth()  # Get new auth token
+        ring = Ring(auth)
+
+    await ring.async_update_data()
+
+    print(ring.devices())
+    await auth.async_close()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+    
+""" OLD
 def main():
     if cache_file.is_file():
         
@@ -47,3 +86,4 @@ def main():
 
 if __name__ == "__main__":
     ring = main()
+"""
